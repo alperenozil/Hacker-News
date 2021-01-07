52e6130f90d1e2dabf0ee32924f0eea63a3b8528
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-07 14:40:16 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Necdet Alperen Özil on 7.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
