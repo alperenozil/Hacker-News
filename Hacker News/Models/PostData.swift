c24ae075bbfa7f921392c211e800f257e77b1a04
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-09 10:19:27 +0300

    Update PostData.swift

--- Hacker News/Models/PostData.swift
+++ Hacker News/Models/PostData.swift
@@ -1,16 +1,4 @@
 
 import Foundation
 
-struct Results: Decodable{
-    let hits: [Post]
-}
 
-struct Post: Decodable, Identifiable {
-    var id: String {
-        return objectID
-    }
-    let objectID: String
-    let points: Int
-    let title: String
-    let url: String
-}
