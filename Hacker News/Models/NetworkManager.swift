a473741767674f802359f161dbd606703f4c6346
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-15 21:19:26 +0300

    Update NetworkManager.swift

--- Hacker News/Models/NetworkManager.swift
+++ Hacker News/Models/NetworkManager.swift
@@ -28,18 +28,4 @@ class NetworkManager: ObservableObject {
     }
 }
 
-struct Results: Decodable{
-    var data: data
-}
 
-struct data: Decodable{
-    var content: [Post]
-}
-
-struct Post: Decodable, Identifiable {
-
-    var id: Int
-    let shopId: Int
-    let barcodeNumber: String
-    let name: String
-}
